fb82dc8f133abceaf48390606926a0ffd087296a
José Fernandes <8190239@estg.ipp.pt>
2021-04-14 21:06:41 +0100

    UPDATE

--- Ficha 3/Parte 2/fp3-master/toDoSample/js/main.js
+++ Ficha 3/Parte 2/fp3-master/toDoSample/js/main.js
@@ -1,44 +1,71 @@
 const tasks = ["Estudar PAW"];
 
-function removeFromList(task){
-    // ToDo
+function removeFromList(task) {
+    if (task) {
+        let index = tasks.indexOf(task);
+        tasks.splice(index, 1);
+    }
 }
 
-function addToList(task){
+function addToList(task) {
     tasks.push(task);
-    // ToDo
 }
 
-function addTask(){
-    // ToDo
+function addTask() {
+    const task = document.getElementById("taskText").value;
+    if (task) {
+        addToList(task);
+        createItemTaskInHtml(task);
+    }
 }
 
-function loadFromStorage(taskList){
+function loadFromStorage(taskList) {
     //ToDo load from the storage api
 }
 
-function saveInStorage(taskList){
+function saveInStorage(taskList) {
     //ToDo save taskList in the storage api
 }
 
-function removeTask(ev){
-    // ToDo
-}
-
-function createItemTaskInHtml(task){
-    // ToDo
-}
-
-
-
-function init(){
-    document.getElementById('addButton').addEventListener("click", addTask );
-    document.body.addEventListener("load", ()=>{
-        tasks.forEach( function(item){
-            createItemTaskInHtml(item);
-        })
-    })
+function removeTask(task) {
+    removeFromList(task);
+    let articles = document.getElementById("taskList").childNodes;
     
+    for(let i = 0; i < articles.length; i++){
+        if(articles[i].firstChild){
+            articles[i].childNodes[1].removeEventListener;
+            articles[i].parentNode.removeChild(articles[i]);
+            return;
+        }
+    }
+}
+
+function createItemTaskInHtml(task) {
+    let taskList = document.getElementById("taskList");
+    let article = document.createElement("article");
+    let taskElem = document.createElement("span");
+    taskElem.innerText = task;
+    taskElem.classList.add("taskText");
+
+    let closeButtom = document.createElement("button");
+    closeButtom.innerText = "X";
+    closeButtom.classList.add("taskClose");
+    closeButtom.addEventListener("click", function (ev) {
+        ev.preventDefault();
+        removeTask(task);
+    })
+
+    article.appendChild(taskElem);
+    article.appendChild(closeButtom);
+    taskList.appendChild(article);
+}
+
+function init() {
+    document.getElementById('addButton').addEventListener("click", addTask);
+    tasks.forEach(function (item) {
+        createItemTaskInHtml(item);
+    })
+
 }
 
 init();
--- Ficha 3/Parte 2/fp3-master/validationSample/js/main.js
+++ Ficha 3/Parte 2/fp3-master/validationSample/js/main.js
@@ -1,9 +1,36 @@
-document.getElementById('submeter').addEventListener("click", function (ev) {
 
-});
+document.getElementById("submeter").addEventListener("click", function (ev) {
+    validateFromData(ev);
+})
 
 
-validateFromData(){
+function validateFromData(ev) {
     ev.preventDefault();
-    // ToDo
+    debugger
+    const user = document.getElementsByName("uname")[0].value;
+    const pass1 = document.getElementById("pass1").value;
+    const pass2 = document.getElementById("pass2").value;
+
+    if (user.length < 7) {
+        alert("Username invalid (too short)");
+        return false;
+    }
+
+    if (pass1 !== pass2) {
+        alert("Diferent passwords!");
+        return false;
+    }
+
+    if (pass1.lenght < 6) {
+        alert("Short passwords!");
+        return false;
+    }
+
+    let reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/g
+    if (!pass1.match(reg)) {
+        alert("Invalid password");
+        return false;
+    }
+
+    alert("Successful login");
 }
