5fbb0875d57d16e9605fe263a7cd9651b4846bad
José Fernandes <8190239@estg.ipp.pt>
2021-04-23 21:16:43 +0100

    Retoques na ficha 5

--- Ficha 5/Parte 1/routes/books.js
+++ Ficha 5/Parte 1/routes/books.js
@@ -1,32 +1,90 @@
 var express = require('express');
+var querystring = require('querystring');
 var fs = require('fs');
 var path = require('path');
 var router = express.Router();
 
+router.get('/review', function (req, res) {
+    console.log(req.query);
+
+    let errors = [];
+
+    if (req.query.errors instanceof Array) {
+        errors = req.query.errors;
+    } else {
+        errors = [req.query.errors];
+    }
+
+    const params = req.query;
+    delete params.errors;
+
+    console.log("Entering render phase");
+    console.log(errors);
+    console.log(params);
+
+    res.render('index.ejs', { title: 'Review', errors, params })
+});
+
 /* POST book review form */
 router.post('/review', function (req, res, next) {
-    console.log(req.body);
     let params = {
         title: 'Book Review',
         ...req.body
     };
 
-    let bookName = `${params.email}_${params.book}`;
+    const errors = [];
 
+    if (!params.book) {
+        errors.push("Missing book field");
+    } else if (params.book.trim() === '') {
+        errors.push("Book name cannot be blank");
+    }
+
+    if (!params.name) {
+        errors.push("Missing name field");
+    } else {
+        if (params.name.trim() === '') {
+            errors.push("Name cannot be blank");
+        }
+    }
+
+    if (!params.email) {
+        errors.push("Missing email field");
+    } else {
+        if (params.email.trim() === '') {
+            errors.push("Email cannot be blank");
+        }
+    }
+
+    if (!params.readed) {
+        errors.push("Missing read or not read option")
+    }
+
+    if (errors.length > 0) {
+        const query = querystring.stringify({
+            errors,
+            name: params.name,
+            email: params.email,
+            book: params.book,
+            description: params.description,
+            readed: params.readed
+        });
+        return res.redirect('/books/review?' + query);
+    }
+
+    let bookName = `${params.email}_${params.book}`;
     let bookReviewPath = path.join(__dirname, '../data', bookName);
 
     fs.writeFileSync(bookReviewPath, JSON.stringify(params));
 
-    res.status(200).render('bookReview', params);
+    res.render('bookReview', params);
 });
 
 /* GET book review */
 router.get('/review/:email/:livro', function (req, res, next) {
-    console.log(req.params.email);
-    console.log(req.params.livro);
-
     const bookName = `${req.params.email}_${req.params.livro}`;
     const bookReviewPath = path.join(__dirname, '../data', bookName);
+
     try {
         const bookReviewJSON = fs.readFileSync(bookReviewPath, 'utf8');
         let review = JSON.parse(bookReviewJSON);
@@ -44,6 +102,7 @@ router.get('/reviews', function (req, res) {
 
     fs.readdirSync(bookReviewsPath).forEach(file => {
         const bookReviewPath = path.join(bookReviewsPath, file);
+
         try {
             const bookReviewJSON = fs.readFileSync(bookReviewPath, 'utf8');
             let review = JSON.parse(bookReviewJSON);
--- Ficha 5/Parte 1/routes/index.js
+++ Ficha 5/Parte 1/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Review' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Review', errors: [] });
 });
 
 module.exports = router;
