f01ed4a2ad96e12ae38219a9dff5cc1f651c3613
José Fernandes <8190239@estg.ipp.pt>
2021-04-24 13:13:08 +0100

    Ex 5 completo

--- Ficha 5/Parte 1/routes/books.js
+++ Ficha 5/Parte 1/routes/books.js
@@ -3,8 +3,20 @@ var querystring = require('querystring');
 var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
+var reviewsDir = 'data'
+var reviewsPath = path.join(__dirname, '..', 'public', reviewsDir);
 
-var data = multer({ dest: 'data/' });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/data/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, getImageName(req, file));
+    }
+});
+
+var data = multer({ storage: storage });
 
 var router = express.Router();
 
@@ -34,7 +46,7 @@ router.get('/review', function (req, res) {
 
 /* POST book review form */
 router.post('/review', data.single('image'), function (req, res) {
-    console.log(req.file);
+    // console.log(req.file);
     const image = req.file;
     const allowedImgExtensions = ['.jpg', '.png'];
 
@@ -72,6 +84,7 @@ router.post('/review', data.single('image'), function (req, res) {
     }
 
     if (image) {
+        params.imagePath = `/${reviewsDir}/${getImageName(req, req.file)}`;
         const fileExtension = path.extname(image.originalname);
         if (!allowedImgExtensions.includes(fileExtension)) {
             errors.push("Invalid File Extension")
@@ -93,23 +106,19 @@ router.post('/review', data.single('image'), function (req, res) {
     const reviewID = `${params.email}_${params.book}`;
 
     const bookName = reviewID + '.json';
-    const bookReviewPath = path.join(__dirname, '../data', bookName);
-
-    const imageExtension = path.extname(image.originalname);
-    const imageName = reviewID + imageExtension;
-    req.file.filename = imageName;
-    req.file.path = path.join('data', imageName);
-    console.log(req.file);
+    const bookReviewPath = path.join(reviewsPath, bookName);
 
     fs.writeFileSync(bookReviewPath, JSON.stringify(params));
+
+    console.log(params);
 
     res.render('bookReview', params);
 });
 
 /* GET book review */
 router.get('/review/:email/:livro', function (req, res, next) {
-    const bookName = `${req.params.email}_${req.params.livro}`;
-    const bookReviewPath = path.join(__dirname, '../data', bookName);
+    const bookName = `${req.params.email}_${req.params.livro}.json`;
+    const bookReviewPath = path.join(reviewsPath, bookName);
 
     try {
         const bookReviewJSON = fs.readFileSync(bookReviewPath, 'utf8');
@@ -124,10 +133,9 @@ router.get('/review/:email/:livro', function (req, res, next) {
 router.get('/reviews', function (req, res) {
 
     let reviews = [];
-    const bookReviewsPath = path.join(__dirname, '../data');
 
-    fs.readdirSync(bookReviewsPath).forEach(file => {
-        const bookReviewPath = path.join(bookReviewsPath, file);
+    fs.readdirSync(reviewsPath).forEach(file => {
+        const bookReviewPath = path.join(reviewsPath, file);
 
         try {
             const bookReviewJSON = fs.readFileSync(bookReviewPath, 'utf8');
@@ -139,5 +147,12 @@ router.get('/reviews', function (req, res) {
 
     res.status(200).render('bookTable', { reviews: reviews });
 });
+
+function getImageName(req, file) {
+    const reviewID = `${req.body.email}_${req.body.book}`;
+    const imageExtension = path.extname(file.originalname);
+    const imageName = reviewID + imageExtension;
+    return imageName;
+}
 
 module.exports = router;
